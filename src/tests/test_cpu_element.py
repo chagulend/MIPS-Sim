94b4e8a28db235f4bf4f334a353f6e8fdf518203
Christopher Auglend <chris.auglend@gmail.com>
2018-11-16 15:43:52 +0100

    Renamed left to source and right to result. Now have a basic test of read input.

--- src/tests/test_cpu_element.py
+++ src/tests/test_cpu_element.py
@@ -1,39 +1,44 @@
 import pytest
 from cpu_element import CPU_element
 
+source = "source"
+result = "result"
+
 
 class Test_CPU_element:
     def test_init(self):
-        left = "Left"
-        right = "Right"
         with pytest.raises(TypeError):
             CPU_element()
-            CPU_element(left)
-            CPU_element(left, right)
+            CPU_element(source)
+            CPU_element(source, result)
             CPU_element([1], [2])
-        cpu = CPU_element([left], [right])
-        assert left in cpu.inputs
-        assert left not in cpu.outputs
-        assert right in cpu.outputs
-        assert right not in cpu.inputs
+        cpu = CPU_element([source], [result])
+        assert source in cpu.inputs
+        assert source not in cpu.outputs
+        assert result in cpu.outputs
+        assert result not in cpu.inputs
 
     def test_connect(self):
-        left = "left"
-        right = "right"
-        a = CPU_element([left], [right])
-        b = CPU_element([right], [left])
+        a = CPU_element([], [result])
+        b = CPU_element([result], [])
         with pytest.raises(TypeError):
             b.connect(a)
             b.connect(5)
             b.connect([5])
         b.connect([a])
-        assert b.input_sources[right] == a
+        assert b.input_sources[result] == a
         with pytest.raises(KeyError):
-            b.input_sources[left]
-            b.connect([CPU_element([], [right])])
+            b.input_sources[source]
+            b.connect([CPU_element([], [result])])
 
     def test_read_input(self):
-        pass
+        a = CPU_element([], [result])
+        b = CPU_element([result], [])
+        i = 42
+        b.connect([a])
+        a.outputs[result] = i
+        b.read_inputs()
+        assert b.inputs[result] == i
 
     def test_write_output(self):
         with pytest.raises(NotImplementedError):
