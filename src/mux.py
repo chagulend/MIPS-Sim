eb37e61b00f406f23a81def8d5fe186b358f59c8
Christopher Auglend <chris.auglend@gmail.com>
2018-11-16 18:11:22 +0100

    Write outputs implemented.

--- src/mux.py
+++ src/mux.py
@@ -2,4 +2,11 @@ from cpu_element import CPU_element
 
 
 class Mux(CPU_element):
-    pass
+    def write_outputs(self):
+        control = self.inputs[self.input_names[0]]
+        result = self.output_names[0]
+        print(control, result)
+        if control:
+            self.outputs[result] = self.inputs[self.input_names[2]]
+        else:
+            self.outputs[result] = self.inputs[self.input_names[1]]
