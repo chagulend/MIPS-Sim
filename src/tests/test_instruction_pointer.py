54ebbb366ca00e521603a5bb760ac9f122507765
Christopher Auglend <chris.auglend@gmail.com>
2018-11-18 10:43:43 +0100

    Test for write_outputs implemented.

--- src/tests/test_instruction_pointer.py
+++ src/tests/test_instruction_pointer.py
@@ -1,6 +1,6 @@
 from cpu_element import CPU_element
 from instruction_pointer import Instruction_pointer
-
+from tests.tools import set_signals
 
 signals = "input"
 result = "result"
@@ -10,7 +10,9 @@ class Test_Instruction_pointer:
     source = CPU_element([], [signals])
     ip = Instruction_pointer([signals], [result])
     assert isinstance(ip, CPU_element)
-    ip.connect(source)
+    ip.connect([source])
 
     def test_write_output(self):
-        pass
+        value = 55
+        set_signals(self.source, self.ip, [signals], [value])
+        assert ip.output[result] == value
