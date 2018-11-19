cba750c5c4b3f257deba62b5a1ca7dd81b90d6f5
Christopher Auglend <chris.auglend@gmail.com>
2018-11-19 13:50:57 +0100

    write_outputs implemented.

--- src/instruction_pointer.py
+++ src/instruction_pointer.py
@@ -2,4 +2,7 @@ from cpu_element import CPU_element
 
 
 class Instruction_pointer(CPU_element):
-    pass
+    def write_outputs(self):
+        input_name = self.input_names[0]
+        result = self.output_names[0]
+        self.outputs[result] = self.inputs[input_name]
