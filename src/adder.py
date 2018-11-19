4d02af2d8268d3ed4b73e7ce0277580e2c8a95b1
Christopher Auglend <chris.auglend@gmail.com>
2018-11-19 14:18:07 +0100

    write_output implemented.

--- src/adder.py
+++ src/adder.py
@@ -2,4 +2,6 @@ from cpu_element import CPU_element
 
 
 class Adder(CPU_element):
-    pass
+    def write_outputs(self):
+        result = self.output_names[0]
+        self.outputs[result] = sum(self.inputs.values()) & 0xffffffff
