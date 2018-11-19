9b02abc025fa286a477f78d13acf01f995c91b5b
Christopher Auglend <chris.auglend@gmail.com>
2018-11-19 14:48:34 +0100

    Moving small CPU_element subclasses into elements.py.

--- src/adder.py
+++
@@ -1,7 +0,0 @@
-from cpu_element import CPU_element
-
-
-class Adder(CPU_element):
-    def write_outputs(self):
-        result = self.output_names[0]
-        self.outputs[result] = sum(self.inputs.values()) & 0xffffffff
--- src/instruction_pointer.py
+++
@@ -1,8 +0,0 @@
-from cpu_element import CPU_element
-
-
-class Instruction_pointer(CPU_element):
-    def write_outputs(self):
-        input_name = self.input_names[0]
-        result = self.output_names[0]
-        self.outputs[result] = self.inputs[input_name]
