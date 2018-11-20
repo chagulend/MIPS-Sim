d665030decbfb669b0cd0f74b412763cff7178f4
Christopher Auglend <chris.auglend@gmail.com>
2018-11-20 19:51:59 +0100

    Removed empty test.

--- src/tests/test_register_file.py
+++ src/tests/test_register_file.py
@@ -4,8 +4,6 @@ from elements import Register_file
 
 class Test_Register_file:
     def test_initial_state(self):
-        assert Register_file([], []).registers == {i:0 for i in range(32)}
-
-    def test_write_outputs(self):
-        raise NotImplementedError
+        a = Register_file([], [])
+        assert a.registers == {i:0 for i in range(32)}
         
