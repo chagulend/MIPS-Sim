d8aa611d4eedaddbd07cd24d3cfc4f12d3f58e9d
Christopher Auglend <chris.auglend@gmail.com>
2018-11-20 19:14:02 +0100

    Redesigned the tests to properly test read_inputs and control. Extended testing of init to assert that inputs are not set.

--- src/tests/test_constant.py
+++ src/tests/test_constant.py
@@ -13,6 +13,9 @@ class Test_Constant:
             Constant(2.2, result)
         with pytest.raises(TypeError):
             Constant(value, 8)
+        a = Constant(value, result)
+        assert a.inputs == {}
+        assert a.input_names == []
 
     def test_write_outputs(self):
         a = Constant(value, result)
@@ -20,10 +23,12 @@ class Test_Constant:
         assert a.outputs[result] == value
 
     def test_connect(self):
-        with pytest.raises(TypeError):
-            a = Constant(value, result)
-            a.connect(CPU_element([], []))
+        a = Constant(value, result)
+        a.connect([a])
+        assert a.input_sources == dict()
 
     def test_read_inputs(self):
         a = Constant(value, result)
+        inputs = a.inputs.copy()
         a.read_inputs()
+        assert inputs == a.inputs
