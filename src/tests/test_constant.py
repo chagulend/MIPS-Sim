f0019402053c9fe07f7be75ca5d26786af6696fb
Christopher Auglend <chris.auglend@gmail.com>
2018-12-03 20:24:59 +0100

    Updated tests for new argument order of Constant.

--- src/tests/test_constant.py
+++ src/tests/test_constant.py
@@ -12,23 +12,23 @@ class Test_Constant:
         with pytest.raises(TypeError):
             Constant(2.2, result)
         with pytest.raises(TypeError):
-            Constant(value, 8)
-        a = Constant(value, result)
+            Constant(value, result)
+        a = Constant(result, value)
         assert a.inputs == {}
         assert a.input_names == []
 
     def test_write_outputs(self):
-        a = Constant(value, result)
+        a = Constant(result, value)
         a.write_outputs()
         assert a.outputs[result] == value
 
     def test_connect(self):
-        a = Constant(value, result)
+        a = Constant(result, value)
         a.connect([a])
         assert a.input_sources == dict()
 
     def test_read_inputs(self):
-        a = Constant(value, result)
+        a = Constant(result, value)
         inputs = a.inputs.copy()
         a.read_inputs()
         assert inputs == a.inputs
