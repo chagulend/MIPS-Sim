7a58cb352e9943cf03f717324ab1ad0bab6ae7be
Christopher Auglend <chris.auglend@gmail.com>
2018-12-04 23:31:53 +0100

    Including Register_file. Updated status method.

--- src/mips_simulator.py
+++ src/mips_simulator.py
@@ -2,6 +2,7 @@
 from cpu_element import CPU_element
 from elements import Adder, Mux, Instruction_pointer, Constant
 from memory import Data_memory, Instruction_memory
+from register_file import Register_file
 
 
 class MIPS_simulator:
@@ -11,6 +12,7 @@ class MIPS_simulator:
         self.ip = Instruction_pointer(["Ins_incr"], ["Ins_out"])
         self.data_memory = Data_memory([], [])
         self.instruction_memory = Instruction_memory([], [])
+        self.register_file = Register_file([], [])
         elements.append(self.ip)
         elements.append(Constant("Constant", 4))
         elements.append(Constant("Control", 1))
@@ -38,8 +40,11 @@ class MIPS_simulator:
         self.ticks += 1
 
     def status(self):
+        """ Returns the status of the simulator as a printable string."""
         outputs = ["Total ticks: {}".format(self.ticks)]
         outputs.append(self.ip.status())
+        outputs.append(self.register_file.status())
+        outputs.append(self.data_memory.status())
         outputs.append("Result: {}"
                        .format(self.elements[-1].outputs["Result"]))
         return "\n".join(outputs)
@@ -49,4 +54,4 @@ if __name__ == "__main__":
     simulator = MIPS_simulator()
     for i in range(10):
         simulator.tick()
-        print(simulator.status())
+    print(simulator.status())
