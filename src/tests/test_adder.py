3aac477fe2f970223bff9d8b942b60e878c34885
Christopher Auglend <chris.auglend@gmail.com>
2018-12-07 16:13:42 +0100

    Now only using a function for testing.

--- src/tests/test_adder.py
+++ src/tests/test_adder.py
@@ -7,17 +7,15 @@ sources = ["a", "b"]
 result = "result"
 
 
-class Test_Adder:
-    a = CPU_element([], sources)
-    b = Adder(sources, [result])
-    assert isinstance(b, CPU_element)
-    b.connect([a])
-
-    def test_write_outputs(self):
-        a = 42
-        b = 8
-        set_signals(self.a, self.b, sources, [a, b])
-        assert self.b.outputs[result] == a + b
-        a = 2**32 - 1
-        set_signals(self.a, self.b, sources, [a, b])
-        assert self.b.outputs[result] == (a + b) & a
+def test_write_outputs(self):
+    source = CPU_element([], sources)
+    adder = Adder(sources, [result])
+    assert isinstance(adder, CPU_element)
+    adder.connect([source])
+    a = 42
+    b = 8
+    set_signals(source, adder, sources, [a, b])
+    assert self.adder.outputs[result] == a + b
+    a = 2**32 - 1
+    set_signals(source, adder, sources, [a, b])
+    assert self.adder.outputs[result] == (a + b) & a
