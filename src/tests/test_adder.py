c91136043f741aae2690b82e276c1525a609ecdf
Christopher Auglend <chris.auglend@gmail.com>
2018-12-07 16:25:50 +0100

    Renamed sources to signals and moved test to a function.

--- src/tests/test_adder.py
+++ src/tests/test_adder.py
@@ -3,19 +3,19 @@ from elements import Adder
 from tests.tools import set_signals
 
 
-sources = ["a", "b"]
+signals = ["a", "b"]
 result = "result"
 
 
-def test_write_outputs(self):
-    source = CPU_element([], sources)
-    adder = Adder(sources, [result])
+def test_write_outputs():
+    source = CPU_element([], signals)
+    adder = Adder(signals, [result])
     assert isinstance(adder, CPU_element)
     adder.connect([source])
     a = 42
     b = 8
-    set_signals(source, adder, sources, [a, b])
-    assert self.adder.outputs[result] == a + b
+    set_signals(source, adder, signals, [a, b])
+    assert adder.outputs[result] == a + b
     a = 2**32 - 1
-    set_signals(source, adder, sources, [a, b])
-    assert self.adder.outputs[result] == (a + b) & a
+    set_signals(source, adder, signals, [a, b])
+    assert adder.outputs[result] == (a + b) & a
