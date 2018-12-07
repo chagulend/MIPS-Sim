d8e27b74310d3d1d9166f218e3f2c3b0f94c7dfc
Christopher Auglend <chris.auglend@gmail.com>
2018-12-07 16:25:02 +0100

    Test is now a simple function. Signals now is a list.

--- src/tests/test_instruction_pointer.py
+++ src/tests/test_instruction_pointer.py
@@ -2,17 +2,15 @@ from cpu_element import CPU_element
 from elements import Instruction_pointer
 from tests.tools import set_signals
 
-signals = "input"
+signals = ["address"]
 result = "result"
 
 
-class Test_Instruction_pointer:
-    source = CPU_element([], [signals])
-    ip = Instruction_pointer([signals], [result])
+def test_write_output():
+    source = CPU_element([], signals)
+    ip = Instruction_pointer(signals, [result])
     assert isinstance(ip, CPU_element)
     ip.connect([source])
-
-    def test_write_output(self):
-        value = 55
-        set_signals(self.source, self.ip, [signals], [value])
-        assert self.ip.outputs[result] == value
+    value = 55
+    set_signals(source, ip, signals, [value])
+    assert ip.outputs[result] == value
