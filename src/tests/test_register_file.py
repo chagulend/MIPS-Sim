ec830af243547f7b0ec7cd66faf2a0ecafe101bb
Christopher A <chris.auglend@gmail.com>
2022-05-18 12:23:12 +0200

    Modifying test for initial state of register of Register_file to reflect change from dict to list.

--- src/tests/test_register_file.py
+++ src/tests/test_register_file.py
@@ -5,4 +5,4 @@ from register_file import Register_file
 class Test_Register_file:
     def test_initial_state(self):
         a = Register_file([], [])
-        assert a.registers == {i:0 for i in range(32)}
+        assert a.registers == [0 for _ in range(32)]
