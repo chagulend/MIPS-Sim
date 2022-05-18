48a33760859e20fc27b55784a6dceb2f994afeb9
Christopher A <chris.auglend@gmail.com>
2022-05-18 12:12:57 +0200

    Adding newlines to Register_file for readability.

--- src/register_file.py
+++ src/register_file.py
@@ -8,16 +8,20 @@ class Register_file(CPU_element):
 
     def status(self):
         """ Returns the registry as a printable string."""
-        register_names = ["$zero", "$at", "$v0", "$v1", "$a0",
-                          "$a1", "$a2", "$a3", "$t0", "$t1",
-                          "$t2", "$t3", "$t4", "$t5", "$t6",
-                          "$t7", "$s0", "$s1", "$s2", "$s3",
-                          "$s4", "$s5", "$s6", "$s7", "$t8",
-                          "$t9", "$k0", "$k1", "$gp", "$sp",
-                          "$fp", "$ra"]
+        register_names = [
+            "$zero", "$at", "$v0", "$v1", "$a0",
+            "$a1", "$a2", "$a3", "$t0", "$t1",
+            "$t2", "$t3", "$t4", "$t5", "$t6",
+            "$t7", "$s0", "$s1", "$s2", "$s3",
+            "$s4", "$s5", "$s6", "$s7", "$t8",
+            "$t9", "$k0", "$k1", "$gp", "$sp",
+            "$fp", "$ra"
+        ]
         outputs = ["---Register file---"]
         for i, name in enumerate(register_names):
             value = self.registers[i]
-            outputs.append("Name: {} \t Hex value: 0x{:08x} \t Value: {}."
-                           .format(name, value, value))
+            outputs.append(
+                "Name: {} \t Hex value: 0x{:08x} \t Value: {}."
+                .format(name, value, value)
+            )
         return "\n".join(outputs)
